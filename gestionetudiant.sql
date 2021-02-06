9051cd488cc0062cdd605bcc4dc4d43662921639
Staratwars <69220160+Staratwars@users.noreply.github.com>
2021-02-06 13:49:37 +0100

    Add files via upload

--- gestionetudiant.sql
+++ gestionetudiant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : sam. 06 fév. 2021 à 11:17
+-- Généré le : sam. 06 fév. 2021 à 13:49
 -- Version du serveur :  10.4.17-MariaDB
 -- Version de PHP : 8.0.1
 
@@ -20,6 +20,46 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `gestionetudiant`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cours`
+--
+
+CREATE TABLE `cours` (
+  `idC` int(255) NOT NULL,
+  `nomCours` varchar(255) NOT NULL,
+  `duree` int(11) NOT NULL,
+  `optionC` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `cours`
+--
+
+INSERT INTO `cours` (`idC`, `nomCours`, `duree`, `optionC`) VALUES
+(1, 'POO', 3, 'Semaine');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `groupe`
+--
+
+CREATE TABLE `groupe` (
+  `idG` int(11) NOT NULL,
+  `GNom` varchar(255) NOT NULL,
+  `annee` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `groupe`
+--
+
+INSERT INTO `groupe` (`idG`, `GNom`, `annee`) VALUES
+(1, 'L3 MIAGE', 2021),
+(2, 'M1 MIAGE', 2023);
 
 -- --------------------------------------------------------
 
@@ -41,11 +81,24 @@ CREATE TABLE `utilisateur` (
 --
 
 INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `motdepasse`, `utype`, `Username`) VALUES
-(8, 'noureddine', 'test', 'a', 'Etudiant', 'testnoureddine');
+(8, 'noureddine', 'test', 'a', 'Etudiant', 'testnoureddine'),
+(9, 'bekhdadi', 'noureddine', 'azerty', 'Etudiant', 'noureddine.bekhdadi');
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `cours`
+--
+ALTER TABLE `cours`
+  ADD PRIMARY KEY (`idC`);
+
+--
+-- Index pour la table `groupe`
+--
+ALTER TABLE `groupe`
+  ADD PRIMARY KEY (`idG`);
 
 --
 -- Index pour la table `utilisateur`
@@ -58,10 +111,22 @@ ALTER TABLE `utilisateur`
 --
 
 --
+-- AUTO_INCREMENT pour la table `cours`
+--
+ALTER TABLE `cours`
+  MODIFY `idC` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `groupe`
+--
+ALTER TABLE `groupe`
+  MODIFY `idG` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
